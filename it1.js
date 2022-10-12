05746d6c8601f08d4d12b906acb3e179a830ba72
ricardograna <ricardogranarosales@gmail.com>
2022-10-12 22:00:43 +0200

    iteracion3

--- it1.js
+++ it1.js
@@ -1,3 +1,9 @@
-showButton = document.querySelector(".showme");
-console.log(showButton);
+const showButton$$ = document.querySelector(".showme");
+console.log(showButton$$);
+
+const h1$$ = document.querySelector("#pillado");
+console.log(h1$$);
+
+const p$$ = document.querySelectorAll("p")
+console.log(p$$);
 
